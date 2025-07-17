28c150cb7e1a9b577ef986a37826355a6eebb385
Sandeshhegde-371 <sandesh66622@gmail.com>
2025-07-17 15:20:51 +0530

    Project structure and venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sandesh Hegde",
+    author_email="sandesh66622@gmail.com",
+    packages=find_packages()
+)
